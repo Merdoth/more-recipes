5640094fc2cf6cf340a554ce96e04eb433905835
Merdoth <ucheya97@gmail.com>
2017-09-28 14:23:12 +0100

    Modifies router

--- server/controller/recipes.js
+++ server/controller/recipes.js
@@ -2,6 +2,10 @@ class Recipe {
   static test (req, res) {
     res.status(200).send('Hello World');
   }
+
+  static add (req, res) {
+    res.status(200).send('Added new route');
+  }
 }
 
-export default Recipe;
+export default Recipe; 
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -4,5 +4,6 @@ import recipeController from '../controller/recipes';
 let router = express.Router();
 
 router.get('/', recipeController.test);
+router.get('/add', recipeController.add);
 
 export default router;
