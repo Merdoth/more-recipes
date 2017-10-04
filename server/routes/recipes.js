d48da467497cc1ab0cd01eec394cdc7f09a592c4
Merdoth <ucheya97@gmail.com>
2017-10-04 18:36:57 +0100

    get recipe route and get recipe controller

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -4,9 +4,9 @@ import recipes from '../controller/recipes';
 let router = express.Router();
 
 //User route goes here
-//router.get('/', user.getAll);
+
 router.post('/', recipes.add);
-// router.get('/', recipes.get);
+router.get('/', recipes.get);
 // router.put('/:Id', recipes.update);
 
 export default router;
