35d9d34960addb1270d9e9373e20942cf42187e3
Chimereucheya Okereke <chimereucheya@Merdoth.local>
2017-11-14 18:15:17 +0100

    feat(signin):  Implement user signin validation -refractor signup route(bug fixing)

--- server/middleware/signUpValidator.js
+++ server/middleware/signUpValidator.js
@@ -1,14 +1,17 @@
 export default function(req, res, next) {
     const{ userName, email, password } = req.body;
 
-        if( !userName || userName == '' ) {
+        if( !userName || userName.trim() == '' ) {
             return res.status(400).send({message: 'Please enter a valid userName!'})
         }
-        if( !email || email == '' ) {
+        if( !email || email.trim() == '' ) {
             return res.status(400).send({message: 'Please enter a valid email!'})
         }
-        if( !password || password == '' ) {
+        if( !password || password .trim() == '' ) {
             return res.status(400).send({message: 'Please enter a valid password!'})
+        }  
+        if( password.length < 8 ) {
+            return res.status(400).send({message: 'Password must be up to 8 characters!'})
         }
     next();    
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ import User from '../controller/user';
 import express from 'express';
 import validateInput from '../shared/validations/signup' ;
 import signUpValidator from '../middleware/signUpValidator';
+import signInValidator from '../middleware/signInValidator';
 
 
 let router = express.Router();
@@ -9,10 +10,9 @@ let router = express.Router();
 
 const userRoutes = (router) => {
   router .get('/users', User.getAllUsers);
-  router.post('/users/signup', User.signUp);
   router .get('/users/:id', User.getOneUser);
   router.post('/users/signup', signUpValidator, User.signUp);
-  router.post('/users/signin', User.signIn);
+  router.post('/users/signin', signInValidator, User.signIn);
 
   router.post('/', (req, res) => {
     const { errors, isValid } = validateInput(req.body);
