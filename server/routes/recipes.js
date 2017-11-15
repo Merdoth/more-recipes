3bfd8645b96d41fe8fd75cdc82b46cd822af243d
Chimereucheya Okereke <chimereucheya@Merdoth.local>
2017-11-15 16:13:03 +0100

    feat(getAllMyRecipes): adds a function that allows a user get all his/her recipes. - enabled route for user getAllMy recipes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -5,6 +5,7 @@ import recipeValidator from '../middleware/recipeValidator';
 const recipeRoutes = (router) => {
   router.post('/recipes/', auth.authorize, recipeValidator, recipes.add);
   router.get('/recipes/', recipes.get);
+  router.get('/recipes/userId', recipes.getMine);
   router.put('/recipes/:id', auth.authorize, recipes.update);
   router.delete('/recipes/:id', auth.authorize, recipes.delete);
 };
