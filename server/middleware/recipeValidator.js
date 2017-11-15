8aa9378584f8b349a86c9c590cc229bcf126e76d
Chimereucheya Okereke <chimereucheya@Merdoth.local>
2017-11-15 16:28:25 +0100

    fix(bug): just a little change

--- server/middleware/recipeValidator.js
+++ server/middleware/recipeValidator.js
@@ -1,14 +1,14 @@
 export default function(req, res, next) {
-    const{ recipeName, ingredients, preparation } = req.body;
+  const{ recipeName, ingredients, preparation } = req.body;
 
-        if( !recipeName || recipeName.trim() == '' ) {
-            return res.status(400).send({ message: 'Please enter a valid recipename!'})
-        }
-        if( !ingredients || ingredients.trim() == '' ) {
-            return res.status(400).send({ message: 'Please enter valid ingredients!'})
-        }
-        if( !preparation || preparation.trim() == '' ) {
-            return res.status(400).send({ message: 'Please enter valid preparation!'})
-        }
-    next();
+  if( !recipeName || recipeName.trim() == '' ) {
+    return res.status(400).send({ message: 'Please enter a valid recipename!'});
+  }
+  if( !ingredients || ingredients.trim() == '' ) {
+    return res.status(400).send({ message: 'Please enter valid ingredients!'});
+  }
+  if( !preparation || preparation.trim() == '' ) {
+    return res.status(400).send({ message: 'Please enter valid preparation!'});
+  }
+  next();
 }
